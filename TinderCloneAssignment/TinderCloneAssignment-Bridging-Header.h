d695cf99d96998937080d2348e345abefccddc89
Yung Dai <yungchidai@gmail.com>
2015-05-25 15:37:46 -0400

    commit

---
+++ TinderCloneAssignment/TinderCloneAssignment-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  TinderCloneAssignment-Bridging-Header.h
+//  TinderCloneAssignment
+//
+//  Created by Yung Dai on 2015-05-25.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#ifndef TinderCloneAssignment_TinderCloneAssignment_Bridging_Header_h
+#define TinderCloneAssignment_TinderCloneAssignment_Bridging_Header_h
+
+// If you are using Facebook, uncomment this line to get automatic import of the header inside your project.
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+#endif
