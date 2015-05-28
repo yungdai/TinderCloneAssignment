b50b36504647eb97790e6dc4e914a019375c92ca
Yung Dai <yungchidai@gmail.com>
2015-05-28 13:16:08 -0400

    add so that when I touch anything when the keyboard is up, that I will resign first responder.

--- TinderCloneAssignment/MyInformationViewController.swift
+++ TinderCloneAssignment/MyInformationViewController.swift
@@ -25,7 +25,8 @@ class MyInformationViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var customImage: UIImageView!
     @IBOutlet weak var moreAboutMeTextBox: UITextView!
     
-    @IBOutlet weak var bottomContraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+
     @IBOutlet weak var parseUserName: UILabel!
     @IBOutlet weak var parseUserEmail: UILabel!
     
@@ -37,19 +38,44 @@ class MyInformationViewController: UIViewController, UITextViewDelegate {
             menuButton.action = "revealToggle:"
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
             
-            // set up NSNotificationCenter for the keyboard listener functions
-            NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
-            NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide", name: UIKeyboardWillHideNotification, object: nil)
+//            // set up NSNotificationCenter for the keyboard listener functions
+//            NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+//            NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide", name: UIKeyboardWillHideNotification, object: nil)
 
         }
     }
-    
-    func keyboardWillShow(notification: NSNotification) {
-        
-    }
-    
-    func keyboardWillHide(notificaiton: NSNotification) {
-        
+//    
+//    func keyboardWillShow(notification: NSNotification) {
+//        adjustingHeight(true, notification: notification)
+//    }
+//    
+//    func keyboardWillHide(notification: NSNotification) {
+//        adjustingHeight(false, notification: notification)
+//    }
+//    
+//    func adjustingHeight(show:Bool, notification:NSNotification) {
+//        // 1
+//        var userInfo = notification.userInfo!
+//        // 2
+//        var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+//        // 3
+//        var animationDurarion = userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSTimeInterval
+//        // 4
+//        var changeInHeight = (CGRectGetHeight(keyboardFrame) + 40) * (show ? 1 : -1)
+//        //5
+//        UIView.animateWithDuration(animationDurarion, animations: { () -> Void in
+//            self.bottomConstraint.constant += changeInHeight
+//        })
+//        
+//    }
+//    
+//    override func viewWillDisappear(animated: Bool) {
+//        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+//        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+//    }
+    // Add following method to close keyboard when touched on empty area
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -91,6 +117,10 @@ class MyInformationViewController: UIViewController, UITextViewDelegate {
         println("User information is saved to Parse!")
     }
     
+    
+    override func touchesCancelled(touches: Set<NSObject>!, withEvent event: UIEvent!) {
+        moreAboutMeTextBox.resignFirstResponder()
+    }
     
     // move the UITextView up when you're about to type something into it.
     
