e22f3bfb8ede582464cf87eb934ef5bb923ffdc2
Yung Dai <yungchidai@gmail.com>
2015-05-29 20:30:16 -0400

    work in progress for the swiping functions

--- TinderCloneAssignment/MainAppViewController.swift
+++ TinderCloneAssignment/MainAppViewController.swift
@@ -18,7 +18,6 @@ class MainAppViewController: UIViewController {
     
     enum CardSelectionState{
         case NoSelection
-        case MakingSelection
         case SwipingLeft
         case SwipedLeft
         case SwipingRight
@@ -28,17 +27,21 @@ class MainAppViewController: UIViewController {
     @IBOutlet weak var cardBackgroundView: UIView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var moreAboutMeBox: UITextView!
-  
+    @IBOutlet weak var noButton: UIImageView!
+    @IBOutlet weak var yesButton: UIImageView!
     
-    
+    var matchesMade = []
+    var currentMatchIndex = 1
+    var currentMatch: String?
+    var listOfRequests = []
+    var currentLocation: PFGeoPoint?
     var cardSelectionState:CardSelectionState = .NoSelection
+    
     @IBOutlet weak var cardImage: UIImageView!
     
     var frame: CGRect!
     var xFromCenter:CGFloat = 0
-
-    let locationManager = CLLocationManager()
-
+    
     // assign the menu button to the viewcontroller
 
     @IBOutlet weak var menuButton: UIBarButtonItem!
@@ -47,7 +50,7 @@ class MainAppViewController: UIViewController {
         super.viewDidLoad()
         frame = CGRectZero
         
-//        cardImage.image = UIImage(named: "mario.jpg")
+
         // giving the image a circle style
         // add a corner radius to our image
         cardImage.layer.cornerRadius = cardImage.frame.size.width / 2
@@ -59,7 +62,6 @@ class MainAppViewController: UIViewController {
             let moreAboutMe = currentUser["moreAboutMe"] as? String {
                 self.nameLabel.text = first_name
                 self.moreAboutMeBox.text = moreAboutMe
-//
                 // parse the photo URL into data for the UIImageView
                 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), { () -> Void in
                     let data = NSData(contentsOfURL: NSURL(string: urlString)!)
@@ -70,22 +72,61 @@ class MainAppViewController: UIViewController {
                 
         }
         // get the current location and sent to Parse
-        PFGeoPoint.geoPointForCurrentLocationInBackground { (geoPoint:PFGeoPoint?, error:NSError?) -> Void in
-            if let user = PFUser.currentUser() {
+        
+        PFGeoPoint.geoPointForCurrentLocationInBackground { (geoPoint:PFGeoPoint?, error:NSError?) -> Void in if let user = PFUser.currentUser() {
+            if (error != nil) {
+                
+            }
+                self.currentLocation = geoPoint
+            if let currentLocation = self.currentLocation {
+                
+            }
                 user["currentLocation"] = geoPoint
                 user.saveInBackground()
+                self.checkForMatches(self.currentMatchIndex, aroundGeopoint: geoPoint!)
             }
             
         }
         
-        
-
         // this code is for the SWRevealViewController Code API
         let revealVC = self.revealViewController()
         if revealVC != nil{
             menuButton.target = revealVC
             menuButton.action = "revealToggle:"
             self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        }
+    }
+    
+    // itterating through a bunch of people that match up
+    func checkForMatches(iterator: Int, aroundGeopoint: PFGeoPoint) {
+        
+        // look into the User class object in Parse
+        var kQuery = PFQuery(className: "_User")
+        // tell parse to check their location and get the nearest users within 10km
+        kQuery.whereKey("currentLocation", nearGeoPoint: aroundGeopoint, withinKilometers: 10)
+        // ask parse  to get the User objects and process them using the parse API
+        kQuery.findObjectsInBackgroundWithBlock { (users: [AnyObject]? , error: NSError?) -> Void in
+            if (error != nil) {
+                // take the current array of user objects and assign them to matchesMade
+                self.matchesMade = users!
+                let closestUser: AnyObject = self.matchesMade
+                let name = closestUser["first_name"] as? String
+                let photo = closestUser["photo"] as? String
+                let photoURL = NSURL(string: photo!)
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), { () -> Void in
+                    let imageData = NSData(contentsOfURL: photoURL!)
+                    dispatch_sync(dispatch_get_main_queue(), {() -> Void in
+                        self.cardImage.image = UIImage(data: imageData!)
+                        self.cardImage.layer.cornerRadius = 8
+                        self.cardImage.clipsToBounds = true
+                        self.currentMatchIndex = iterator
+                        self.currentMatch = closestUser.objectId
+                    })
+                })
+            } else {
+                println("Ooops there's an error")
+                println(error?.description)
+            }
         }
     }
     
@@ -133,25 +174,28 @@ class MainAppViewController: UIViewController {
             }
         }
         if sender.state == UIGestureRecognizerState.Ended {
-            // set the label back
-            //            profile.center = CGPointMake(view.bounds.width / 2, view.bounds.height / 2)
-            //            // undo scale
-            //            scale = max(abs(xFromCenter) / 100, 1)
-            //            // undo rotation and stretch
-            //            rotation = CGAffineTransformMakeRotation(0)
-            //            // stretch the current view
-            //            stretch = CGAffineTransformScale(rotation, scale, scale)
-            //            // set image to original size after scaling
-            
             UIView.animateWithDuration(0.3, animations:
                 { () -> Void in
                     profile.frame = self.frame
                 }, completion: {
-                    (success) -> Void in
-                    self.cardSelectionState = .NoSelection
+                    (completed) -> Void in
+                    if (completed) {
+                        
+                        switch self.cardSelectionState {
+                        case .SwipingLeft:
+                            println("Swiping Left")
+                        case .SwipedLeft:
+                            self.nopeSelected() // need to create the nopeSelected function
+                        case .SwipingRight:
+                            println("Swiping Right")
+                        case .SwipedRight:
+                            self.okSelected() // need to create the okSelected function
+                        default:
+                            println("Oh NO!")
+                        }
+                        self.cardSelectionState = .NoSelection
+                    }
             })
-            
-            
         }
         // TODO: load next image
         
